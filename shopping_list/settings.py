f6d91605799fe125e3970916da4f6334ef634fc8
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2023-09-04 14:15:29 +0700

    Fixed Allowed Host

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["*"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-2532dt$e9q!kpud7hw6deq#4qi8jdgwkp=n^lr1xuj=xc9mm&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
